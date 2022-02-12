55ae55cbe679b9df378c3dc62a717393ee9855dc
Peter Nguyen <ptn045@ucsd.edu>
2022-02-11 17:00:16 -0800

    rename plugin, files, variables

--- nguyen-test/trunk/includes/class-nguyen-test-activator.php
+++ nguyen-test/trunk/includes/class-nguyen-test-activator.php
@@ -6,8 +6,8 @@
  * @link       http://example.com
  * @since      1.0.0
  *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/includes
+ * @package    Nguyen_Test
+ * @subpackage Nguyen_Test/includes
  */
 
 /**
@@ -16,11 +16,11 @@
  * This class defines all code necessary to run during the plugin's activation.
  *
  * @since      1.0.0
- * @package    Plugin_Name
- * @subpackage Plugin_Name/includes
+ * @package    Nguyen_Test
+ * @subpackage Nguyen_Test/includes
  * @author     Your Name <email@example.com>
  */
-class Plugin_Name_Activator {
+class Nguyen_Test_Activator {
 
 	/**
 	 * Short Description. (use period)
--- nguyen-test/trunk/public/class-nguyen-test-public.php
+++ nguyen-test/trunk/public/class-nguyen-test-public.php
@@ -1,35 +1,35 @@
 <?php
 
 /**
- * The admin-specific functionality of the plugin.
+ * The public-facing functionality of the plugin.
  *
  * @link       http://example.com
  * @since      1.0.0
  *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/admin
+ * @package    Nguyen_Test
+ * @subpackage Nguyen_Test/public
  */
 
 /**
- * The admin-specific functionality of the plugin.
+ * The public-facing functionality of the plugin.
  *
  * Defines the plugin name, version, and two examples hooks for how to
  * enqueue the admin-specific stylesheet and JavaScript.
  *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/admin
+ * @package    Nguyen_Test
+ * @subpackage Nguyen_Test/public
  * @author     Your Name <email@example.com>
  */
-class Plugin_Name_Admin {
+class Nguyen_Test_Public {
 
 	/**
 	 * The ID of this plugin.
 	 *
 	 * @since    1.0.0
 	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
+	 * @var      string    $nguyen_test    The ID of this plugin.
 	 */
-	private $plugin_name;
+	private $nguyen_test;
 
 	/**
 	 * The version of this plugin.
@@ -44,18 +44,18 @@ class Plugin_Name_Admin {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
+	 * @param      string    $nguyen_test       The name of the plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
-	public function __construct( $plugin_name, $version ) {
+	public function __construct( $nguyen_test, $version ) {
 
-		$this->plugin_name = $plugin_name;
+		$this->nguyen_test = $nguyen_test;
 		$this->version = $version;
 
 	}
 
 	/**
-	 * Register the stylesheets for the admin area.
+	 * Register the stylesheets for the public-facing side of the site.
 	 *
 	 * @since    1.0.0
 	 */
@@ -65,20 +65,20 @@ class Plugin_Name_Admin {
 		 * This function is provided for demonstration purposes only.
 		 *
 		 * An instance of this class should be passed to the run() function
-		 * defined in Plugin_Name_Loader as all of the hooks are defined
+		 * defined in Nguyen_Test_Loader as all of the hooks are defined
 		 * in that particular class.
 		 *
-		 * The Plugin_Name_Loader will then create the relationship
+		 * The Nguyen_Test_Loader will then create the relationship
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/plugin-name-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->nguyen_test, plugin_dir_url( __FILE__ ) . 'css/nguyen-test-public.css', array(), $this->version, 'all' );
 
 	}
 
 	/**
-	 * Register the JavaScript for the admin area.
+	 * Register the stylesheets for the public-facing side of the site.
 	 *
 	 * @since    1.0.0
 	 */
@@ -88,15 +88,15 @@ class Plugin_Name_Admin {
 		 * This function is provided for demonstration purposes only.
 		 *
 		 * An instance of this class should be passed to the run() function
-		 * defined in Plugin_Name_Loader as all of the hooks are defined
+		 * defined in Nguyen_Test_Loader as all of the hooks are defined
 		 * in that particular class.
 		 *
-		 * The Plugin_Name_Loader will then create the relationship
+		 * The Nguyen_Test_Loader will then create the relationship
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/plugin-name-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->nguyen_test, plugin_dir_url( __FILE__ ) . 'js/nguyen-test-public.js', array( 'jquery' ), $this->version, false );
 
 	}
 
