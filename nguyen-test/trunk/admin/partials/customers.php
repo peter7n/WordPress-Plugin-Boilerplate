fbe82c182e7c46fe22b4ab5254484e5770d25a33
Peter Nguyen <ptn045@ucsd.edu>
2022-02-13 20:13:17 -0800

    clean up html

--- nguyen-test/trunk/admin/partials/customers.php
+++ nguyen-test/trunk/admin/partials/customers.php
@@ -24,35 +24,35 @@ $arr = json_decode($response, TRUE);
 <div class="wrap">
 	<h1>View Customers</h1>
 
-<div style="overflow-x:auto;">
-	<table class="nguyen-table">
-		<tr>
-			<th>Name</th>
-			<th>Username</th>
-         <th>Email</th>
-			<th>Address</th>
-			<th>Phone</th>
-			<th>Website</th>
-			<th>Company</th>
-	</tr>
-	<?php foreach ($arr as $item) { ?>
-		<tr>
-			<td><?php echo $item['name'] ?></td>
-			<td><?php echo $item['username'] ?></td>
-         <td><?php echo $item['email'] ?></td>
-			<td><?php echo $item['address']['street'] ?><br>
-				 <?php echo $item['address']['suite'] ?><br>
-				 <?php echo $item['address']['city'] ?><br>
-				 <?php echo $item['address']['zipcode'] ?>
-			</td>
-			<td><?php echo $item['phone'] ?></td>
-			<td><a href="https://<?php echo $item['website'] ?>"><?php echo $item['website'] ?></a></td>
-			<td><?php echo $item['company']['name'] ?><br>
-				 <?php echo $item['company']['catchPhrase'] ?><br>
-				 <?php echo $item['company']['bs'] ?>
-			</td>
-		</tr>
-	<?php } ?>
-	</table>
-</div>
+	<div style="overflow-x:auto;">
+		<table class="nguyen-table">
+			<tr>
+				<th>Name</th>
+				<th>Username</th>
+	         <th>Email</th>
+				<th>Address</th>
+				<th>Phone</th>
+				<th>Website</th>
+				<th>Company</th>
+			</tr>
+		<?php foreach ($arr as $item) { ?>
+			<tr>
+				<td><?php echo $item['name'] ?></td>
+				<td><?php echo $item['username'] ?></td>
+	         <td><?php echo $item['email'] ?></td>
+				<td><?php echo $item['address']['street'] ?><br>
+					 <?php echo $item['address']['suite'] ?><br>
+					 <?php echo $item['address']['city'] ?><br>
+					 <?php echo $item['address']['zipcode'] ?>
+				</td>
+				<td><?php echo $item['phone'] ?></td>
+				<td><a href="https://<?php echo $item['website'] ?>"><?php echo $item['website'] ?></a></td>
+				<td><?php echo $item['company']['name'] ?><br>
+					 <?php echo $item['company']['catchPhrase'] ?><br>
+					 <?php echo $item['company']['bs'] ?>
+				</td>
+			</tr>
+		<?php } ?>
+		</table>
+	</div>
 </div>
